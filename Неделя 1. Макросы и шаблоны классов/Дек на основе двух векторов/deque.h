515b587958a81f7d3dd027bd1eb8094a3f5e9a3c
PatriotRossii <patriotrossii2019@mail.ru>
2022-01-19 21:54:33 +0500

    Rewrite deque more clean way

--- Неделя 1. Макросы и шаблоны классов/Дек на основе двух векторов/deque.h
+++ Неделя 1. Макросы и шаблоны классов/Дек на основе двух векторов/deque.h
@@ -5,9 +5,55 @@
 
 template<typename T>
 class Deque {
-	std::vector<std::pair<size_t, T>> front;
-	std::vector<std::pair<size_t, T>> back;
-	int currentIdx = 0;
+	enum class Location {
+		FrontVector,
+		BackVector
+	};
+
+	std::vector<T> front;
+	std::vector<T> back;
+
+	Location GetElementLocation(size_t index) const {
+		if(index >= front.size()) {
+			return Location::BackVector;
+		} else {
+			return Location::FrontVector;
+		}
+	}
+
+	size_t GetFrontIndex(size_t index) const {
+		return front.size() - 1 - index;
+	}
+	size_t GetBackIndex(size_t index) const {
+		return index - front.size();
+	}
+
+	typename std::vector<T>::iterator GetElementIterator(size_t index) {
+		size_t targetIndex = 0;
+		switch(GetElementLocation(index)) {
+			case Location::FrontVector:
+				targetIndex = GetFrontIndex(index);
+				if(targetIndex >= front.size()) return front.end();
+				return front.begin() + targetIndex;
+			case Location::BackVector:
+				targetIndex = GetBackIndex(index);
+				if(targetIndex >= back.size()) return back.end();
+				return back.begin() + targetIndex;
+		}
+	}
+	typename std::vector<T>::const_iterator GetElementIterator(size_t index) const {
+		size_t targetIndex = 0;
+		switch(GetElementLocation(index)) {
+			case Location::FrontVector:
+				targetIndex = GetFrontIndex(index);
+				if(targetIndex >= front.size()) return front.cend();
+				return front.cbegin() + targetIndex;
+			case Location::BackVector:
+				targetIndex = GetBackIndex(index);
+				if(targetIndex >= back.size()) return back.cend();
+				return back.cbegin() + targetIndex;
+		}
+	}
 public:
 	Deque() { }
 
@@ -19,67 +65,46 @@ public:
 	}
 
 	T& operator[](size_t index) {
-		return this->operator[](index);
+		return *GetElementIterator(index);
 	}
 	const T& operator[](size_t index) const {
-		auto it = std::find_if(front.begin(), front.end(), [index](const auto& pair) {
-			return pair.first == index;
-		});
-		if(it != front.end()) {
-			return it->second;
-		}
-
-		it = std::find_if(back.begin(), back.end(), [index](const auto& pair) {
-			return pair.first == index;
-		});
-		if(it != back.end()) {
-			return it->second;
-		}
+		return *GetElementIterator(index);
 	}
 
 	T& At(size_t index) {
-		return this->At(index);
+		auto it = GetElementIterator(index);
+		if(it == front.end() || it == back.end()) {
+			throw std::out_of_range{std::string("Element with index ") + std::to_string(index) + " is out of range"};
+		}
+		return *it;
+
 	}
 	const T& At(size_t index) const {
-		auto it = std::find_if(front.begin(), front.end(), [index](const auto& pair) {
-			return pair.first == index;
-		});
-		if(it != front.end()) {
-			return it->second;
+		auto it = GetElementIterator(index);
+		if(it == front.end() || it == back.end()) {
+			throw std::out_of_range{std::string("Element with index ") + std::to_string(index) + " is out of range"};
 		}
-
-		it = std::find_if(back.begin(), back.end(), [index](const auto& pair) {
-			return pair.first == index;
-		});
-		if(it != back.end()) {
-			return it->second;
-		}
-
-		throw std::out_of_range{"Out of range"};
+		return *it;
 	}
 
 	T& Front() {
-		if(!front.empty()) return front.back().second;
-		return back.front().second;
+		return *GetElementIterator(0);
 	}
 	const T& Front() const {
-		if(!front.empty()) return front.back().second;
-		return back.front().second;
+		return *GetElementIterator(0);
 	}
 
 	T& Back() {
-		if(!back.empty()) return back.back().second;
-		return front.front().second;
+		return *GetElementIterator(Size() - 1);
 	}
 	const T& Back() const {
-		if(!back.empty()) return back.back().second;
-		return front.front().second;
+		return *GetElementIterator(Size() - 1);
 	}
 
 	void PushFront(const T& value) {
-		front.push_back({currentIdx++, value});
+		front.push_back(value);
 	}
 	void PushBack(const T& value) {
-		back.push_back({currentIdx++, value});
+		back.push_back(value);
 	}
 };
