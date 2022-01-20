2dc75a312b93bcfc7864ff980a303ae065607767
PatriotRossii <patriotrossii2019@mail.ru>
2022-01-20 19:47:48 +0500

    Try to make 'Плохой макрос' solution correct

--- Неделя 1. Макросы и шаблоны классов/Плохой макрос/print_values.cpp
+++ Неделя 1. Макросы и шаблоны классов/Плохой макрос/print_values.cpp
@@ -3,10 +3,11 @@
 #include <ostream>
 using namespace std;
 
-#define PRINT_VALUES(out, x, y) {	\
-	std::ostream& stream = out;		\
-	stream << (x) << (std::endl);	\
-	stream << (y) << (std::endl);	\
+#define PRINT_VALUES(out, x, y) {                 \
+  std::ostream& adsolqwei1 = (out);               \
+  const auto& asd2389 = (x);                      \
+  const auto& asdilj18 = (y);                     \
+	adsolqwei1 << asd2389 << std::endl << asdilj18 << std::endl;  \
 }
 
 int main() {
@@ -16,5 +17,17 @@ int main() {
     PRINT_VALUES(output, 5, "red belt");
     ASSERT_EQUAL(output.str(), "5\nred belt\n");
   }, "PRINT_VALUES usage example");
+
+  if(true) {
+    PRINT_VALUES(std::cout, 10, "MY FIRST TEST")
+  } else {
+    PRINT_VALUES(std::cout, 10, "MY FIRST TEST AGAIN")
+  }
+
+  if(true)
+    PRINT_VALUES(std::cout, 10, "MY SECOND TEST")
+  else
+    PRINT_VALUES(std::cout, 10, "MY SECOND TEST AGAIN")
+
   return 0;
 }
