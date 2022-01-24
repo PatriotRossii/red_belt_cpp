2bc0fec6fee5cf7b413583257822a329932aa099
PatriotRossii <patriotrossii2019@mail.ru>
2022-01-24 20:55:41 +0500

    Implement 'Система бронирования отелей' complete

--- Неделя 2. Принципы оптимизации кода, сложность алгоритмов и эффективное использование ввода и вывода/Система бронирования отелей/main.cpp
+++ Неделя 2. Принципы оптимизации кода, сложность алгоритмов и эффективное использование ввода и вывода/Система бронирования отелей/main.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 template<typename C, typename K, typename V>
-auto insertOrIncrease(C& container, const K& key, const V& value) {
-	if(auto it = c.find(key); it != c.end()) {
+typename C::iterator insertOrIncrease(C& container, const K& key, const V& value) {
+	if(auto it = container.find(key); it != container.end()) {
 		it->second += value;
 		return it;
 	} else {
-		return container.insert({key, value});
+		return container.insert({key, value}).first;
 	}
 
 }
@@ -17,14 +17,17 @@ class HotelManager {
 	unordered_map<string, int> rooms;
 	unordered_map<string, map<int, int>> clients;
 
-	set<long long, unordered_map<string, int>::iterator> roomsEvents;
-	set<long long, unordered_map<string, map<int, int>>::iterator> clientsEvents;
-
+	map<long long, vector<pair<string, map<int, int>::iterator>>> events;
 	void Adjust(long long time) {
-		long long lowerBound = time - 86000;
-		for(auto it = roomsEvents.begin(), end = roomsEvents.end(); it != end; ) {
+		long long lowerBound = time - 86400;
+
+		for(auto it = events.begin(), end = events.end(); it != end; ) {
 			if(it->first <= lowerBound) {
-				it = roomsEvents.erase(it);
+				for(auto& [hotelName, clientIterator]: it->second) {
+					rooms[hotelName] -= clientIterator->second;
+					clients[hotelName].erase(clientIterator);
+				}
+				it = events.erase(it);
 			} else {
 				++it;
 			}
@@ -32,37 +35,47 @@ class HotelManager {
 	}
 public:
 	void Book(long long time, const string& hotelName, int clientId, int roomCount) {
-		Adjust();
-		roomsEvents.insert(
-			time, HotelManager::insertOrIncrease(rooms, hotelName, roomCount)
-		);
-		clientsEvent.insert(
-			time, HotelManager::insertOrIncrease(clients[hotelName], clientId, roomCount)
-		);
+		Adjust(time);
+
+		rooms[hotelName] += roomCount;
+		pair<string, map<int, int>::iterator> clientsNode =
+			{hotelName, insertOrIncrease(clients[hotelName], clientId, roomCount)};
+		events[time].push_back(clientsNode);
 	}
-	void CountOfClients(const string& hotel) const {
-		return clients[hotel].size();
+	int CountOfClients(const string& hotel) const {
+		if(clients.count(hotel)) {
+			return clients.at(hotel).size();
+		}
+		return 0;
 	}
 	int CountOfRooms(const string& hotel) const {
-		return rooms[hotel];
+		if(rooms.count(hotel)) {
+			return rooms.at(hotel);
+		}
+		return 0;
 	}
-}
+};
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
+	HotelManager manager;
 	int countOfQueries; cin >> countOfQueries;
+
 	while(countOfQueries--) {
 		string command; cin >> command;
 		if(command == "BOOK") {
 			long long time; cin >> time;
-			string hotel_name; cin >> hotel_name;
-			int client_id, room_count; cin >> client_id >> room_count;
+			string hotelName; cin >> hotelName;
+			int clientId, roomCount; cin >> clientId >> roomCount;
+			manager.Book(time, hotelName, clientId, roomCount);
 		} else if(command == "CLIENTS") {
-			string hotel_name; cin >> hotel_name;
+			string hotelName; cin >> hotelName;
+			cout << manager.CountOfClients(hotelName) << '\n';
 		} else if(command == "ROOMS") {
-			string hotel_name; cin >> hotel_name;
+			string hotelName; cin >> hotelName;
+			cout << manager.CountOfRooms(hotelName) << '\n';
 		}
 	}
 }
