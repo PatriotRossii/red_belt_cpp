6abd26d34ecb6105c76a4aa1fd3e03ed80159179
PatriotRossii <patriotrossii2019@mail.ru>
2022-02-06 20:14:21 +0500

    Add try on 'StackVector'

---
+++ Неделя 4. Эффективное использование линейных контейнеров/StackVector/stack_vector.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <stdexcept>
+#include <array>
+
+using namespace std;
+
+template <typename T, size_t N>
+class StackVector {
+public:
+  explicit StackVector(size_t a_size = 0): size(a_size) {
+    if(a_size > capacity) {
+      throw invalid_argument{"Invalid size"};
+    }
+    for(auto it = array.begin(), end = it + a_size; it != end; ++it) {
+      *it = T();
+    }
+  }
+
+  T& operator[](size_t index) {
+    return array[index];
+  }
+  const T& operator[](size_t index) const {
+    return array[index];
+  }
+
+  typename array<T, N>::iterator begin() {
+    return array.begin();
+  }
+  typename array<T, N>::iterator end() {
+    return array.end();
+  }
+  typename array<T, N>::const_iterator begin() const {
+    return array.cbegin();
+  }
+  typename array<T, N>::const_iterator end() const {
+    return array.cend();
+  }
+
+  size_t Size() const {
+    return size;
+  }
+  size_t Capacity() const {
+    return capacity;
+  }
+
+  void PushBack(const T& value) {
+    if(size == capacity) throw overflow_error{"Can't insert no more"};
+    array[size] = value;
+    size += 1;
+  }
+  T PopBack() {
+    if(size == 0) throw underflow_error{"There are no elements"};
+    size -= 1;
+    return array[size];
+  }
+
+private:
+  array<T, N> array;
+  size_t capacity{N};
+  size_t size{0};
+};
