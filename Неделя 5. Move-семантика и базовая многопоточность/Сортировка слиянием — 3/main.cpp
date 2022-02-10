40e4acc13138a1a3e39b46edccb7a0b438ba88b7
PatriotRossii <patriotrossii2019@mail.ru>
2022-02-10 18:25:01 +0500

    Optimize solution

--- Неделя 5. Move-семантика и базовая многопоточность/Сортировка слиянием — 3/main.cpp
+++ Неделя 5. Move-семантика и базовая многопоточность/Сортировка слиянием — 3/main.cpp
@@ -1,5 +1,5 @@
-#include "test_runner.h"
-//#include "../../common/test_runner.h"
+//#include "test_runner.h"
+#include "../../common/test_runner.h"
 
 #include <iterator>
 #include <algorithm>
@@ -23,32 +23,28 @@ void MergeSort(RandomIt range_begin, RandomIt range_end) {
 	auto begin = elements.begin();
 	auto end = elements.end();
 
-	vector<typename RandomIt::value_type> firstPart{
-		make_move_iterator(begin),
-		make_move_iterator(next(begin, length / 3))
-	};
-	vector<typename RandomIt::value_type> secondPart{
-		make_move_iterator(next(begin, length / 3)),
-		make_move_iterator(next(begin, length / 3 * 2))
-	};
-	vector<typename RandomIt::value_type> thirdPart{
-		make_move_iterator(next(begin, length / 3 * 2)),
-		make_move_iterator(end)
-	};
+	auto firstPartBegin = begin;
+	auto firstPartEnd = next(begin, length / 3);
 
-	MergeSort(firstPart.begin(), firstPart.end());
-	MergeSort(secondPart.begin(), secondPart.end());
-	MergeSort(thirdPart.begin(), thirdPart.end());
+	auto secondPartBegin = firstPartEnd;
+	auto secondPartEnd = next(begin, length / 3 * 2);
+
+	auto thirdPartBegin = secondPartEnd;
+	auto thirdPartEnd = end;
+
+	MergeSort(firstPartBegin, firstPartEnd);
+	MergeSort(secondPartBegin, secondPartEnd);
+	MergeSort(thirdPartBegin, thirdPartEnd);
 
 	vector<typename RandomIt::value_type> temporaryVector;
 	merge(
-		make_move_iterator(firstPart.begin()), make_move_iterator(firstPart.end()),
-		make_move_iterator(secondPart.begin()), make_move_iterator(secondPart.end()),
+		make_move_iterator(firstPartBegin), make_move_iterator(firstPartEnd),
+		make_move_iterator(secondPartBegin), make_move_iterator(secondPartEnd),
 		back_inserter(temporaryVector)
 	);
 	merge(
 		make_move_iterator(temporaryVector.begin()), make_move_iterator(temporaryVector.end()),
-		make_move_iterator(thirdPart.begin()), make_move_iterator(thirdPart.end()),
+		make_move_iterator(thirdPartBegin), make_move_iterator(thirdPartEnd),
 		range_begin
 	);
 }
